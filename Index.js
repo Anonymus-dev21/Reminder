1e4f37b44517364b1947f07bf76f92f8bf3b8d70
Anonymus-dev21 <oscab05@outlook.com>
2025-01-25 10:47:12 -0300

    Express

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ import pkg from "whatsapp-web.js";
 import qrcodeTerminal from "qrcode-terminal";
 import cron from "node-cron";
 import puppeteer from "puppeteer";
+import express from "express";
 const { Client, LocalAuth } = pkg;
 
 import dotenv from "dotenv";
@@ -26,6 +27,18 @@ client.on("qr", (qr) => {
 });
 
 
+const app = express();
+const port = process.env.PORT || 4000;
+app.get("/send", (req, res) => {
+    client.sendMessage(`5493487216141@c.us`, "Generando log").then(() => {
+                console.log("Mensaje enviado");
+            }).catch((error) => {
+                console.error("Error al enviar el mensaje:", error);
+            })
+            res.send("Bot activo");
+        })
+   
+
 
     client.on("ready", () => {
         console.log("Client is ready!");
@@ -39,7 +52,9 @@ client.on("qr", (qr) => {
             })
         })
     })
-
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
     
 
 client.initialize();
